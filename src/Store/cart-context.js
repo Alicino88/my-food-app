7d6c8b0c3799f87ec39ebf8ecc0694de4a19ff4b
Alice Moretti <ali.moret88@gmail.com>
2022-09-21 15:35:04 +0300

    Added comment to cart-context.js

--- src/Store/cart-context.js
+++ src/Store/cart-context.js
@@ -1,5 +1,6 @@
 import React from "react";
 
+//Below I provide a default value for the context (the app works also without assigning one)
 const CartContext = React.createContext({
   items: [],
   totalAmount: 0,
